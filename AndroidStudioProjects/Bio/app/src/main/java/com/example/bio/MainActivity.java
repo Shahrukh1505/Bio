cc5a5f6dfa2dfc8acea31d6c030910aa45094241
shak1505 <shahrukhansari15@gmail.com>
2021-07-08 11:29:29 +0530

    dataBinding chamges

--- AndroidStudioProjects/Bio/app/src/main/java/com/example/bio/MainActivity.java
+++ AndroidStudioProjects/Bio/app/src/main/java/com/example/bio/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.bio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.bio.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
+private EditText enterHobbies;
+private TextView hobbies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+//
+//        enterHobbies = findViewById(R.id.hobbies);
+//        hobbies = findViewById(R.id.hobbiesText);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+   binding.done.setOnClickListener(new View.OnClickListener() {
+       @Override
+       public void onClick(View v) {
+           addHobbies(v);
+       }
+   });
+    }
+
+    public void addHobbies(View view) {
+        binding.hobbiesText.setText("Hobbies: %s" + binding.hobbies.getText().toString().trim());
+binding.hobbies.setVisibility(View.VISIBLE);
+binding.invalidateAll();
+//        hobbies.setText("Hobbies: %s" + enterHobbies.getText().toString().trim());
+//hobbies.setVisibility(View.VISIBLE);
+
+//hide keyboard
+        InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
     }
 }
